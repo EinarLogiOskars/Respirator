11deac86c4bae7b187ac24e630aab054a5d13ab8
Stuart McLean <stuart_mclean86@hotmail.com>
2017-11-29 14:45:22 +0100

    Basic fragments Done

--- app/src/main/java/com/group5/respirator/Communication_akt.java
+++ app/src/main/java/com/group5/respirator/Communication_akt.java
@@ -17,6 +17,8 @@ public class Communication_akt extends AppCompatActivity
     FeelingtStatusFragment FeelingFrag;
     MessageFragment MessageFrag;
     QuestionFragment QuestionFrag;
+    FragmentManager fragmentManager;
+    TextView textView1;
 
 
 
@@ -26,7 +28,78 @@ public class Communication_akt extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communication);
 
+        fragmentManager = getSupportFragmentManager();
+        ActionFrag = ActionsFragment.newInstance();
+        DisplayPainFrag = DisplayPainFragment.newInstance();
+        FeelingFrag = FeelingtStatusFragment.newInstance();
+        MessageFrag = MessageFragment.newInstance();
+        QuestionFrag = QuestionFragment.newInstance();
 
+        textView1 = (TextView) findViewById(R.id.title1);
 
     }
+
+    public void DisplayActionFrag(View view)
+    {
+
+        if (textView1.getVisibility() == View.VISIBLE)
+        {
+            textView1.setVisibility(View.GONE);
+        }
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, ActionFrag);
+        fragmentTransaction.commit();
+    }
+    public void DisplayPainFrag(View view)
+    {
+
+        if (textView1.getVisibility() == View.VISIBLE)
+        {
+            textView1.setVisibility(View.GONE);
+        }
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, DisplayPainFrag);
+        fragmentTransaction.commit();
+    }
+    public void DisplayFeelingFrag(View view)
+    {
+        if (textView1.getVisibility() == View.VISIBLE)
+        {
+            textView1.setVisibility(View.GONE);
+        }
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, FeelingFrag);
+        fragmentTransaction.commit();
+    }
+    public void DisplayMessageFrag(View view) {
+
+        if (textView1.getVisibility() == View.VISIBLE)
+        {
+            textView1.setVisibility(View.GONE);
+        }
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, MessageFrag);
+        fragmentTransaction.commit();
+    }
+    public void DisplayQuestionFrag(View view)
+    {
+        if (textView1.getVisibility() == View.VISIBLE)
+        {
+            textView1.setVisibility(View.GONE);
+        }
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, QuestionFrag);
+        fragmentTransaction.commit();
+    }
+
+    public void home(View view)
+    {
+        finish();
+    }
+
 }
