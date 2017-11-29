804e8491882176e0eaaf6a6b3bff4dfc3dc88036
Stuart McLean <stuart_mclean86@hotmail.com>
2017-11-29 21:33:05 +0100

    Fragment Message part nearly Done

--- app/src/main/java/com/group5/respirator/MessageFragment.java
+++ app/src/main/java/com/group5/respirator/MessageFragment.java
@@ -7,9 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MessageFragment extends Fragment
+public class MessageFragment extends Fragment implements View.OnClickListener
 {
+    private EditText Message;
+    private Button EraseButton;
 
     public MessageFragment()
     {
@@ -26,12 +30,26 @@ public class MessageFragment extends Fragment
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_message, container, false);
+
+        Message = (EditText) view.findViewById(R.id.PatientMessage);
+        EraseButton = (Button) view.findViewById(R.id.EraseButton);
+
+        EraseButton.setOnClickListener(this);
+
         return inflater.inflate(R.layout.fragment_message, container, false);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        Message.getText().clear();
     }
 }
