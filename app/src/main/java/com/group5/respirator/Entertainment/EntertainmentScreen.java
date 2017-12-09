3f2d9ed783faf6eb0850dde9c22c31af6242da34
Bijan Negari <bijiboy_bib_bob@hotmail.com>
2017-12-09 23:56:43 +0100

    Chess game string changed

--- app/src/main/java/com/group5/respirator/Entertainment/EntertainmentScreen.java
+++ app/src/main/java/com/group5/respirator/Entertainment/EntertainmentScreen.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.group5.respirator.Entertainment.Fragments.ChessFragment;
 import com.group5.respirator.Entertainment.Fragments.MemoryFragment;
@@ -14,8 +15,7 @@ import com.group5.respirator.Entertainment.Fragments.QuizFragment;
 import com.group5.respirator.Entertainment.Fragments.TicTacToeFragment;
 import com.group5.respirator.R;
 
-public class EntertainmentScreen extends AppCompatActivity {
-
+public class EntertainmentScreen extends AppCompatActivity implements View.OnClickListener {
 
     FragmentManager fragmentManager;
 
@@ -23,6 +23,10 @@ public class EntertainmentScreen extends AppCompatActivity {
     TicTacToeFragment ticTacToeFrag;
     MemoryFragment memoryFrag;
     ChessFragment chessFrag;
+
+    private Button ticTacToeBtn;
+    private Button quizBtn;
+    private Button memoryBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,15 @@ public class EntertainmentScreen extends AppCompatActivity {
         ticTacToeFrag = TicTacToeFragment.newInstance();
         memoryFrag = MemoryFragment.newInstance();
         chessFrag = ChessFragment.newInstance();
+
+        ticTacToeBtn = (Button) findViewById(R.id.ticTacToeBtn);
+        quizBtn = (Button) findViewById(R.id.quizBtn);
+        memoryBtn = (Button) findViewById(R.id.memoryBtn);
+
+        ticTacToeBtn.setOnClickListener(this);
+        quizBtn.setOnClickListener(this);
+        memoryBtn.setOnClickListener(this);
+
     }
 
     public void openQuiz(View view) {
@@ -71,5 +84,24 @@ public class EntertainmentScreen extends AppCompatActivity {
     public void home(View view) {
 
         finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == ticTacToeBtn){
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, ticTacToeFrag);
+            fragmentTransaction.commit();
+        }
+        else if (view == quizBtn){
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, quizFrag);
+            fragmentTransaction.commit();
+        }
+        else if (view == memoryBtn){
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, memoryFrag);
+            fragmentTransaction.commit();
+        }
     }
 }
