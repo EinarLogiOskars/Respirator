0f451b91eaf2db9b24e9e0fc47c4d84554fd73c8
Bijan Negari <bijiboy_bib_bob@hotmail.com>
2017-12-24 00:08:45 +0100

    Added alertDialog for call button in entertainment screen

--- app/src/main/java/com/group5/respirator/Entertainment/EntertainmentScreen.java
+++ app/src/main/java/com/group5/respirator/Entertainment/EntertainmentScreen.java
@@ -1,13 +1,16 @@
 package com.group5.respirator.Entertainment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.group5.respirator.Entertainment.Fragments.ChessFragment;
 import com.group5.respirator.Entertainment.Fragments.MemoryFragment;
@@ -24,6 +27,7 @@ public class EntertainmentScreen extends AppCompatActivity implements View.OnCli
     MemoryFragment memoryFrag;
     ChessFragment chessFrag;
 
+    private Button callButton;
     private Button ticTacToeBtn;
     private Button quizBtn;
     private Button memoryBtn;
@@ -44,6 +48,7 @@ public class EntertainmentScreen extends AppCompatActivity implements View.OnCli
         memoryFrag = MemoryFragment.newInstance();
         chessFrag = ChessFragment.newInstance();
 
+        callButton = (Button) findViewById(R.id.callButton);
         ticTacToeBtn = (Button) findViewById(R.id.ticTacToeBtn);
         quizBtn = (Button) findViewById(R.id.quizBtn);
         memoryBtn = (Button) findViewById(R.id.memoryBtn);
@@ -51,7 +56,7 @@ public class EntertainmentScreen extends AppCompatActivity implements View.OnCli
         ticTacToeBtn.setOnClickListener(this);
         quizBtn.setOnClickListener(this);
         memoryBtn.setOnClickListener(this);
-
+        callButton.setOnClickListener(this);
     }
 
     public void openQuiz(View view) {
@@ -102,6 +107,38 @@ public class EntertainmentScreen extends AppCompatActivity implements View.OnCli
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.replace(R.id.fragment_container, memoryFrag);
             fragmentTransaction.commit();
+        }
+        else if (view == callButton){
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+            // Setting Alert Dialog Title
+            alertDialogBuilder.setTitle("Konfirmer kald!!!");
+
+            // Icon Of Alert Dialog. Har bare valgt et tilfældigt billed, det ændrer vi
+            alertDialogBuilder.setIcon(R.drawable.thumbs);
+
+            // Setting Alert Dialog Message
+            alertDialogBuilder.setMessage("Are you sure, You want to call?");
+
+            // Man kan cancel alertDialog ved at trykke uden for boksen
+            alertDialogBuilder.setCancelable(true);
+
+            alertDialogBuilder.setPositiveButton("Kald!", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface arg0, int arg1) {
+                    finish();
+                }
+            });
+
+            alertDialogBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getApplicationContext(),"You clicked on Cancel",Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
         }
     }
 }
