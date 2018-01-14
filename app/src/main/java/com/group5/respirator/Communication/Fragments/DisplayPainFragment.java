38042e2d2ba1325ee8c472064a3e50a882e1c40b
Stuart McLean <stuart_mclean86@hotmail.com>
2018-01-14 02:59:21 +0100

    Pain fragment nearly done

--- app/src/main/java/com/group5/respirator/Communication/Fragments/DisplayPainFragment.java
+++ app/src/main/java/com/group5/respirator/Communication/Fragments/DisplayPainFragment.java
@@ -1,17 +1,32 @@
 package com.group5.respirator.Communication.Fragments;
 
-
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.RelativeLayout;
 import com.group5.respirator.R;
+import java.util.ArrayList;
 
 
 public class DisplayPainFragment extends Fragment
 {
+
 
     public DisplayPainFragment()
     {
@@ -28,13 +43,63 @@ public class DisplayPainFragment extends Fragment
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display_pain, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_display_pain, container, false);
+
+        RelativeLayout relativeLayout1 = rootView.findViewById(R.id.rect1);
+        RelativeLayout relativeLayout2 = rootView.findViewById(R.id.rect2);
+
+        relativeLayout1.addView(new Drawboard(getActivity()));
+        relativeLayout2.addView(new Drawboard(getActivity()));
+
+        return rootView;
     }
 
+    private class Drawboard extends View {
+
+        ArrayList<Point> touchPoint = new ArrayList<Point>();
+        Paint paintType = new Paint();
+
+        Bitmap markerIcon = BitmapFactory.decodeResource(getResources(), R.drawable.marker);
+        Bitmap bitmapResized = markerIcon.createScaledBitmap(markerIcon, 25,25,false);
+
+        public Drawboard(Context a)
+        {
+            super(a);
+        }
+        @Override
+        public boolean onTouchEvent(MotionEvent b)
+        {
+            System.out.println(b);
+            Point points = new Point((int) b.getX(), (int) b.getY());
+            touchPoint.add(points);
+            invalidate();
+            return true;
+        }
+
+        @Override
+        public void onDraw(Canvas c) {
+            for (Point p : touchPoint)
+            {
+//                c.drawCircle(p.x, p.y, 3, paintType);
+                c.drawBitmap(bitmapResized, p.x, p.y, paintType);
+            }
+        }
+    }
 }
+
+
+
