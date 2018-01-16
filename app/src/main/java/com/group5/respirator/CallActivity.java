f94496c96cae3eff2be93835cc7556f60fe69482
Bijan Negari <bijiboy_bib_bob@hotmail.com>
2018-01-16 21:16:25 +0100

    Call button now implemented, sound and toast.

--- app/src/main/java/com/group5/respirator/CallActivity.java
+++ app/src/main/java/com/group5/respirator/CallActivity.java
@@ -1,15 +1,20 @@
 package com.group5.respirator;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class CallActivity extends AppCompatActivity implements OnClickListener {
 
     private Button confirmCallButton;
     private Button cancelCallButton;
+    MediaPlayer apple_ring;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +26,31 @@ public class CallActivity extends AppCompatActivity implements OnClickListener {
 
         confirmCallButton.setOnClickListener(this);
         cancelCallButton.setOnClickListener(this);
+
+        apple_ring = MediaPlayer.create(this, R.raw.apple_ring);
+
     }
 
     @Override
     public void onClick(View view) {
         if (view == confirmCallButton){
             // Call effect, sound, something....?
-            System.out.print("Call confirmed pressed");
+            Context context = getApplicationContext();
+            CharSequence text = "Call confirmed - Calling a nurse";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            apple_ring.start();
             finish();
         }
         else if (view == cancelCallButton){
-            System.out.print("Call cancel pressed");
+            Context context = getApplicationContext();
+            CharSequence text = "Call canceled";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
             finish();
         }
     }
