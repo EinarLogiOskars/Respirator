ebf9d3b2e796ee887e05edf826c00c94aaa4c3fc
Unknown <einarlogi1@gmail.com>
2018-01-17 01:14:56 +0100

    Text size settings
    The change text size setting is now functional, just needs to change text size attribute on the textviews its suppose to affect.

--- app/src/main/java/com/group5/respirator/Settings/Fragments/TextSizeFragment.java
+++ app/src/main/java/com/group5/respirator/Settings/Fragments/TextSizeFragment.java
@@ -5,12 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.group5.respirator.Preferences;
 import com.group5.respirator.R;
 
 
-public class TextSizeFragment extends Fragment {
+public class TextSizeFragment extends Fragment implements View.OnClickListener {
 
+    Spinner fontSizeSpinner;
+    Button setSizeBtn;
+    Preferences prefs;
 
     public TextSizeFragment() {
         // Required empty public constructor
@@ -24,6 +32,7 @@ public class TextSizeFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        getActivity().getTheme().applyStyle(new Preferences(getContext()).getFontStyle().getResId(), true);
         super.onCreate(savedInstanceState);
     }
 
@@ -31,7 +40,32 @@ public class TextSizeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_size, container, false);
+        View v = inflater.inflate(R.layout.fragment_text_size, container, false);
+
+        prefs = new Preferences(getContext());
+
+        fontSizeSpinner = (Spinner) v.findViewById(R.id.font_styles);
+        ArrayAdapter<Preferences.FontStyle> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, Preferences.FontStyle.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fontSizeSpinner.setAdapter(adapter);
+
+        //fontSizeSpinner.setSelection(prefs.getFontStyle().ordinal());
+
+        setSizeBtn = (Button) v.findViewById(R.id.setSizeBtn);
+        setSizeBtn.setOnClickListener(this);
+
+        return v;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.setSizeBtn:
+                Toast.makeText(getContext(), "Blablabla", Toast.LENGTH_LONG).show();
+                prefs.setFontStyle((Preferences.FontStyle) fontSizeSpinner.getSelectedItem());
+                break;
+            default:
+                break;
+        }
+    }
 }
